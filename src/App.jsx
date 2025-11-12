fbdd71aafe7d467668e239b980359969ae9022be
dilan12fe <dilanbercalle@gmail.com>
2025-11-11 23:52:06 -0300

    creacion del espacio catalogo y login en front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import Catalogo from "./pages/Catalogo.jsx";
+import Login from "./pages/Logintemp.jsx";
 
-export default function App(){
+export default function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/catalogo" element={<Catalogo />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
