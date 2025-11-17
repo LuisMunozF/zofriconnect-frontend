9ef97584c4309c47b3e7ee37c65598b3f248ea1f
JhoelEsquivel2001 <jhoele366@gmail.com>
2025-11-17 16:59:27 -0300

    +Arrelgo de formulario de registro de usuario

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Catalogo from "./pages/Catalogo.jsx";
 import Login from "./pages/Logintemp.jsx";
 import Empresa from "./pages/Empresa.jsx";
 import Mapa from "./pages/Mapa";
+import Registro from "./pages/Registro.jsx";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/catalogo" element={<Catalogo />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/Registro" element={<Registro />} />
         <Route path="/Empresa" element={<Empresa />} />
         <Route path="/mapa" element={<Mapa/>} />
       </Routes>
