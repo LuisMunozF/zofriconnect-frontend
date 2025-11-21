acb0ea14d26f4a4d1866ea58b807f8bee0116641
LuisMunozF <l.munozf@hotmail.com>
2025-11-21 14:48:18 -0300

    Arreglado la url de Registro

--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -24,7 +24,7 @@ export default function Registro() {
     }
 
     try {
-      const response = await fetch("https://zofriconnect-backend.onrender.com/api/registro/", {
+      const response = await fetch("https://zofriconnect.netlify.app/Registro", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
