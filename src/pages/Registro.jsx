612350401d433334bebba116a6c7eeb2f54779cd
dilan bernal <l.munozf@hotmail.com>
2025-11-21 14:23:54 -0300

    Cambios en la conexion del apartado de registro, ahora esta conectado a render

--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -24,7 +24,7 @@ export default function Registro() {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/registro/", {
+      const response = await fetch("https://zofriconnect-backend.onrender.com/api/registro/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
